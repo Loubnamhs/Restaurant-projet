e80d6951c23f9d5fe2ffbac5e5c1df1604a504c1
loubnamhs <mahsounloubna0@gmail.com>
2023-04-14 16:27:08 +0200

    create table articles

---
+++ sql/create_table_articles.sql
@@ -0,0 +1,6 @@
+Create table La_carte(
+            ArticleID INT AUTO_INCREMENT NOT NULL primary key,
+            Nom_article varchar(100),
+            Categorie varchar(50),
+            Prix FLOAT,
+            Descriptions varchar(200) );
